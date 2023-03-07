b4846068dffad8d1950658ffb4e33242525ffc79
Rafael Fernández López <ereslibre@ereslibre.es>
2023-03-07 13:21:45 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
         devShells = {
           php = pkgs.mkShell {
             buildInputs = with pkgs; [
-              autoconf binaryen bison php re2c wabt
+              autoconf binaryen bison coreutils php re2c wabt wasmtime
             ];
             shellHook = ''
               export WASI_SDK_PATH="${self.packages.${system}.wasi-sdk}"
@@ -54,6 +54,47 @@
               export CC="$WASI_SDK_PATH/bin/clang --sysroot=$WASI_SDK_PATH/share/wasi-sysroot"
               export CFLAGS="-D_WASI_EMULATED_GETPID -D_WASI_EMULATED_SIGNAL -D_WASI_EMULATED_PROCESS_CLOCKS"
               export LDFLAGS="-lwasi-emulated-getpid -lwasi-emulated-signal -lwasi-emulated-process-clocks"
+              export SKIP_IO_CAPTURE_TESTS=1
+              export TEST_PHP_JUNIT=junit.out.xml
+              export STACK_LIMIT_DEFAULTS_CHECK=1
+              export TEST_PHP_EXECUTABLE=$PWD/wasmtime-run-cli.sh
+              export TEST_PHP_CGI_EXECUTABLE=$PWD/wasmtime-run-cgi.sh
+              export TEST_PHPDBG_EXECUTABLE=""
+
+              cat <<-'EOF' > wasmtime-run-cli.sh
+              	#!/usr/bin/env bash
+              	WASMTIME_BACKTRACE_DETAILS=1 wasmtime run --allow-unknown-exports --mapdir /::/ sapi/cli/php.optimized -- "$@"
+              EOF
+              chmod +x wasmtime-run-cli.sh
+              cat <<-'EOF' > wasmtime-run-cgi.sh
+              	#!/usr/bin/env bash
+              	WASMTIME_BACKTRACE_DETAILS=1 wasmtime run --allow-unknown-exports --mapdir /::/ sapi/cgi/php-cgi.optimized -- "$@"
+              EOF
+              chmod +x wasmtime-run-cgi.sh
+
+              configure() {
+                 ./buildconf --force; ./configure --host=wasm32-wasi host_alias=wasm32-musl-wasi --target=wasm32-wasi target_alias=wasm32-musl-wasi --without-iconv --without-openssl --without-libxml --without-pear --disable-phar --disable-opcache --disable-zend-signals --without-pcre-jit --disable-pdo --disable-fiber-asm --disable-posix --without-sqlite3 --disable-dom --disable-xml --disable-simplexml --without-libxml --disable-xmlreader --disable-xmlwriter
+              }
+
+              build() {
+                make -j cgi cli
+              }
+
+              optimize() {
+                wasm-opt -O sapi/cli/php -o sapi/cli/php.optimized
+                wasm-opt -O sapi/cgi/php-cgi -o sapi/cgi/php-cgi.optimized
+              }
+
+              runtests() {
+                php run-tests.php -q \
+                     -j$(nproc) \
+                     -g FAIL,BORK,LEAK,XLEAK \
+                     --no-progress \
+                     --offline \
+                     --show-diff \
+                     --show-slow 1000 \
+                     --set-timeout 120
+              }
           '';
           };
         };
