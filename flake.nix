89c03466854fc0423dfa8ea51c97d80e887c6c5a
Rafael Fernández López <ereslibre@ereslibre.es>
2023-03-13 08:07:45 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -13,17 +13,19 @@
         wasmRuntimes = with pkgs; [ wasmtime ];
         wasmGenericTools = with pkgs; [ binaryen wabt ];
         devGenericTools = with pkgs; [ lldb ];
-        allWasmTools = wasmGenericTools ++ wasmRuntimes ++ devGenericTools
-          ++ (with self.packages.${system}; [ wasi-sdk ]);
+        allWasmTools = wasmGenericTools ++ wasmRuntimes ++ devGenericTools;
       in {
         packages = { wasi-sdk = pkgs.callPackage ./packages/wasi-sdk { }; };
         devShells = {
           nix = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt ]; };
           php = pkgs.callPackage ./shells/php {
-            inherit (self.packages.${system}) wasi-sdk;
             inherit allWasmTools;
+            inherit (self.packages.${system}) wasi-sdk;
           };
-          wasi-sdk = pkgs.mkShell { buildInputs = allWasmTools; };
+          wasi-sdk = pkgs.mkShell {
+            buildInputs = [ allWasmTools ]
+              ++ (with self.packages.${system}; [ wasi-sdk ]);
+          };
         };
       });
 }
