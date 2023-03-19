022292f02f3c5387efe622fe8f50868769423fff
Rafael Fernández López <ereslibre@ereslibre.es>
2023-03-19 21:46:58 +0100

    Update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, flake-utils, nixpkgs }:
