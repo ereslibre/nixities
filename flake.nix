28745461cf64e2930efc84f9733ae1a5779171db
Rafael Fernández López <ereslibre@ereslibre.es>
2023-04-02 13:16:37 +0200

    Update

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
               nativeBuildInputs = allWasmTools;
               shellHook = ''
                 export WASI_SDK_PATH=${wasi-sdk}
-                export CC=${wasi-sdk}/bin/clang
+                export CC="${wasi-sdk}/bin/clang --sysroot=${wasi-sdk}/share/wasi-sysroot"
               '';
             });
           wasm =
