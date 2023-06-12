a6901e547b87d8f8e83dabd5601327739b8c3b81
Rafael Fernández López <ereslibre@ereslibre.es>
2023-06-12 23:53:29 +0200

    Update

--- devenv/templates/nixity/flake.nix
+++ devenv/templates/nixity/flake.nix
@@ -17,20 +17,24 @@
     devShells = forEachSystem (system: let
       pkgs = nixities.legacyPackages.${system};
     in {
+      # The default devShell
       default = devenv.lib.mkShell {
         inherit pkgs;
         inputs.nixpkgs = nixities.nixpkgs;
         modules = [
-          {
+          ({pkgs, ...}: {
             # https://devenv.sh/reference/options/
             packages = with pkgs; [hello];
 
             enterShell = ''
               hello
             '';
-          }
+          })
         ];
       };
+      # Example of another devShell directly forwarded from the
+      # devShells that nixities exposes
+      inherit (nixities.devShells.${system}) wasm;
     });
   };
 }
