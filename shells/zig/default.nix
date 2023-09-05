e39fc96c014648dac6749b03a66056cf3b7eb0f2
Rafael Fernández López <ereslibre@ereslibre.es>
2023-09-05 19:35:38 +0200

    Update

--- shells/zig/default.nix
+++ shells/zig/default.nix
@@ -3,7 +3,11 @@
   cmake,
   libxml2,
   llvmPackages_16,
+  stdenv,
 }:
 mkShell.override {inherit (llvmPackages_16) stdenv;} {
   buildInputs = [cmake libxml2] ++ (with llvmPackages_16; [libclang libllvm lld]);
+  shellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${stdenv.cc.cc.lib}/lib"
+  '';
 }
