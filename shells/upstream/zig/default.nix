3f3cde1fdd9174408ed85cdcea0de495a94a55ab
Rafael Fernández López <ereslibre@ereslibre.es>
2023-09-07 00:23:45 +0200

    Update

--- shells/upstream/zig/default.nix
+++ shells/upstream/zig/default.nix
@@ -1,11 +1,15 @@
 {
   mkShell,
+  gcc,
+  zstd,
   cmake,
   libxml2,
   llvmPackages_16,
   stdenv,
 }:
-mkShell.override {inherit (llvmPackages_16) stdenv;} {
+mkShell {
+  NIX_HARDENING_ENABLE = "";
+  nativeBuildInputs = [gcc zstd];
   buildInputs = [cmake libxml2] ++ (with llvmPackages_16; [libclang libllvm lld]);
   shellHook = ''
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${stdenv.cc.cc.lib}/lib"
