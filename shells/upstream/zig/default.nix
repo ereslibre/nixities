ef88045672cc46dc4867521123a738e9cad8fdfb
Rafael Fernández López <ereslibre@ereslibre.es>
2023-09-10 03:36:40 +0200

    Update

--- shells/upstream/zig/default.nix
+++ shells/upstream/zig/default.nix
@@ -1,26 +1,25 @@
 {
   mkShell,
   stdenv,
-  gcc,
   ninja,
-  zstd,
   cmake,
-  libxml2,
   llvmPackages_16,
+  libxml2,
+  zlib,
   bat,
 }:
 mkShell {
-  LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${stdenv.cc.cc.lib}/lib";
+  # LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${stdenv.cc.cc.lib}/lib";
   NIX_HARDENING_ENABLE = "";
-  nativeBuildInputs = [gcc ninja zstd];
-  buildInputs = [cmake libxml2] ++ (with llvmPackages_16; [libclang libllvm lld]);
+  nativeBuildInputs = [ninja cmake llvmPackages_16.llvm.dev];
+  buildInputs = [libxml2 zlib] ++ (with llvmPackages_16; [libclang lld llvm]);
   shellHook = ''
     ${bat}/bin/bat --language=markdown --decorations=never <<"EOF"
     # Building Zig
 
     First, build the stage3 compiler. Recommended way:
       - `mkdir build; cd build`
-      - `cmake -DCMAKE_BUILD_TYPE=Release -GNinja -DZIG_NO_LIB=ON ..`
+      - `cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_SKIP_BUILD_RPATH=ON -DZIG_STATIC_LLVM=ON -DZIG_TARGET_MCPU=baseline -GNinja -DZIG_NO_LIB=ON ..`
       - `ninja`
 
     This `ninja` step also applies if there were upstream changes.
