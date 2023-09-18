25934d446b810630efa9793636df22a4c732c777
Rafael Fernández López <ereslibre@ereslibre.es>
2023-09-18 15:18:13 +0200

    Update

--- shells/upstream/containerd-wasm-shims/default.nix
+++ shells/upstream/containerd-wasm-shims/default.nix
@@ -8,7 +8,9 @@
   pkg-config,
   protobuf,
   stdenv,
+  zlib,
 }:
 mkShell {
+  LD_LIBRARY_PATH = "${zlib}/lib:$LD_LIBRARY_PATH";
   nativeBuildInputs = [clang cmake containerd pkg-config protobuf openssl];
 }
