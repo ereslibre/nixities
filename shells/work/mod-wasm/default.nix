2eeba92cf14f67911c584f3326f7d13cc51727ff
Rafael Fernández López <ereslibre@ereslibre.es>
2023-09-19 15:22:36 +0200

    Update

--- shells/work/mod-wasm/default.nix
+++ shells/work/mod-wasm/default.nix
@@ -9,5 +9,6 @@ mkShell {
   nativeBuildInputs = [apacheHttpd apacheHttpd.dev apr apr.dev aprutil pkg-config];
   shellHook = ''
     export PATH="${apr.dev}/share/build:$PATH"
+    export LIBTOOL="${apr.dev}/share/build/libtool"
   '';
 }
