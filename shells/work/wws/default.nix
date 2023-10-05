37b739fa78704942bc4a06795d5aecd39338ef03
Rafael Fernández López <ereslibre@ereslibre.es>
2023-10-05 14:52:57 +0200

    Configure plugins.xml

--- shells/work/wws/default.nix
+++ shells/work/wws/default.nix
@@ -10,6 +10,7 @@
   stdenv,
   darwin,
   iconv,
+  writeText,
 }:
 mkShell {
   buildInputs = [wasm-tools];
@@ -19,7 +20,32 @@ mkShell {
     ++ lib.optionals stdenv.isLinux [python3]
     ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [Security SystemConfiguration] ++ [iconv]);
 
-  shellHook = lib.optionalString stdenv.isLinux ''
-    . ${openvino}/setupvars.sh
-  '';
+  shellHook = let
+    openvino_plugins_xml = writeText "plugins.xml" ''
+      <ie>
+          <plugins>
+              <plugin name="AUTO" location="libopenvino_auto_plugin.so">
+              </plugin>
+              <plugin name="BATCH" location="libopenvino_auto_batch_plugin.so">
+              </plugin>
+              <plugin name="CPU" location="libopenvino_intel_cpu_plugin.so">
+              </plugin>
+              <plugin name="GNA" location="libopenvino_intel_gna_plugin.so">
+              </plugin>
+              <plugin name="GPU" location="libopenvino_intel_gpu_plugin.so">
+              </plugin>
+              <plugin name="HETERO" location="libopenvino_hetero_plugin.so">
+              </plugin>
+              <plugin name="MULTI" location="libopenvino_auto_plugin.so">
+              </plugin>
+              <plugin name="MYRIAD" location="libopenvino_intel_myriad_plugin.so">
+              </plugin>
+          </plugins>
+      </ie>
+    '';
+  in
+    lib.optionalString stdenv.isLinux ''
+      export OPENVINO_PLUGINS_XML=${openvino_plugins_xml}
+      . ${openvino}/setupvars.sh
+    '';
 }
