69d71ceb049031b9b7e2d120ee6ed35e2cbd4bfc
Rafael Fernández López <rfernandezl@vmware.com>
2023-10-24 12:26:22 +0200

    Update

--- shells/wasm/default.nix
+++ shells/wasm/default.nix
@@ -1,5 +1,13 @@
 {
+  lib,
   mkShell,
   allWasmTools,
+  stdenv,
+  darwin,
+  iconv,
+  openssl,
 }:
-mkShell {buildInputs = allWasmTools;}
+mkShell {
+  buildInputs = allWasmTools;
+  nativeBuildInputs = [openssl] ++ (lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [Security SystemConfiguration] ++ [iconv]));
+}
