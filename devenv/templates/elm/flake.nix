2e3b174e3c00b6579244328977f06c1b2b508424
Rafael Fernández López <ereslibre@ereslibre.es>
2024-01-01 23:51:59 +0100

    Update

--- devenv/templates/elm/flake.nix
+++ devenv/templates/elm/flake.nix
@@ -31,20 +31,20 @@
           }: {
             env = {
               # `elm-land server` uses this envvar for setting up
-              # listener
+              # its listener
               HOST = "0.0.0.0";
             };
             languages.elm.enable = true;
             packages =
               # We require these binaries to interact with their CLI
               (with pkgs.elmPackages; [elm-land elm-review elm-test])
-              ++ (with pkgs; [just]);
+              ++ (with pkgs; [just tailwindcss tailwindcss-language-server parallel]);
             pre-commit.hooks = {
               elm-format.enable = true;
               elm-review.enable = false;
               elm-test.enable = false;
             };
-            scripts = {
+            scripts = { # User scripts
               init.exec = ''
                 mkdir .app
                 ${pkgs.elmPackages.elm-land}/bin/elm-land init .app
@@ -52,6 +52,16 @@
                 rm .app/.gitignore
                 mv .app/{.*,*} .
                 rmdir .app
+              '';
+              start.exec = ''
+                parallel ::: elm-land-server tailwindcss-watcher
+              '';
+            } // { # Auxiliary scripts
+              elm-land-server.exec = ''
+                elm-land server
+              '';
+              tailwindcss-watcher.exec = ''
+                tailwindcss -i ./tailwind.css -o ./public/styles.css --watch=always
               '';
             };
           })
