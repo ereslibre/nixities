38a63fe64a2f84ce1adefea42151128908474acb
Rafael Fernández López <ereslibre@ereslibre.es>
2024-01-01 14:46:37 +0100

    Update

--- devenv/templates/microvm/flake.nix
+++ devenv/templates/microvm/flake.nix
@@ -35,41 +35,39 @@
       };
     });
 
-    microvms = eachSystem (hostSystem: {
-      host = eachSystem (guestSystem: {
-        guest = nixities.nixpkgs.lib.nixosSystem {
-          system = hostSystem;
-          modules = let
-            pkgs = import nixities.nixpkgs (nixities.nixpkgs.lib.recursiveUpdate {
-                system = hostSystem;
-              } (
-                if guestSystem != hostSystem
-                then {crossSystem.config = guestSystem;}
-                else {}
-              ));
-          in
-            (
+    microvms = eachSystem (hostSystem: (eachSystem (guestSystem: {
+      microvm = nixities.nixpkgs.lib.nixosSystem {
+        system = hostSystem;
+        modules = let
+          pkgs = import nixities.nixpkgs (nixities.nixpkgs.lib.recursiveUpdate {
+              system = hostSystem;
+            } (
               if guestSystem != hostSystem
-              then [{nixpkgs.crossSystem.config = guestSystem;}]
-              else []
-            )
-            ++ [
-              microvm.nixosModules.microvm
-              {
-                microvm = {
-                  cpu =
-                    if guestSystem == "aarch64-linux"
-                    then "cortex-a53"
-                    else null;
-                  hypervisor = "qemu";
-                };
-                environment.systemPackages = with pkgs; [cowsay htop];
-                services.getty.autologinUser = "root";
-                system.stateVersion = "23.11";
-              }
-            ];
-        };
-      });
-    });
+              then {crossSystem.config = guestSystem;}
+              else {}
+            ));
+        in
+          (
+            if guestSystem != hostSystem
+            then [{nixpkgs.crossSystem.config = guestSystem;}]
+            else []
+          )
+          ++ [
+            microvm.nixosModules.microvm
+            {
+              microvm = {
+                cpu =
+                  if guestSystem == "aarch64-linux"
+                  then "cortex-a53"
+                  else null;
+                hypervisor = "qemu";
+              };
+              environment.systemPackages = with pkgs; [cowsay htop];
+              services.getty.autologinUser = "root";
+              system.stateVersion = "23.11";
+            }
+          ];
+      };
+    })));
   };
 }
