ffac9528ad3e63f32a45fa59f8204ed4a2ec075f
Rafael Fernández López <ereslibre@ereslibre.es>
2024-01-01 17:48:09 +0100

    Update

--- devenv/templates/elm/flake.nix
+++ devenv/templates/elm/flake.nix
@@ -35,11 +35,24 @@
               HOST = "0.0.0.0";
             };
             languages.elm.enable = true;
-            packages = with pkgs; [elmPackages.elm-land just];
+            packages =
+              # We require these binaries to interact with their CLI
+              (with pkgs.elmPackages; [elm-land elm-review elm-test])
+              ++ (with pkgs; [just]);
             pre-commit.hooks = {
               elm-format.enable = true;
-              elm-review.enable = true;
-              elm-test.enable = true;
+              elm-review.enable = false;
+              elm-test.enable = false;
+            };
+            scripts = {
+              init.exec = ''
+                mkdir .app
+                ${pkgs.elmPackages.elm-land}/bin/elm-land init .app
+                cat .app/.gitignore >> .gitignore
+                rm .app/.gitignore
+                mv .app/{.*,*} .
+                rmdir .app
+              '';
             };
           })
         ];
