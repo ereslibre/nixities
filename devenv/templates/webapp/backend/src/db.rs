3f26e569d10649208da55fd57a711318a48c24d5
Rafael Fernández López <ereslibre@ereslibre.es>
2024-08-30 19:30:08 +0200

    Update

--- devenv/templates/webapp/backend/src/db.rs
+++ devenv/templates/webapp/backend/src/db.rs
@@ -7,7 +7,7 @@ pub enum DatabaseError {
 
 pub fn identify_database_error(database_error: &dyn SqlxDatabaseError) -> DatabaseError {
     if let Some(error_code) = database_error.code() {
-        if error_code == std::borrow::Cow::Borrowed("2067") {
+        if error_code == "2067" {
             return DatabaseError::UniqueConstraintFailed;
         }
     }
