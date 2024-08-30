6050f6ecb934820321e289d5b6772ccd605c9065
Rafael Fernández López <ereslibre@ereslibre.es>
2024-08-30 14:21:44 +0200

    Update

--- devenv/templates/webapp/backend/migrations/20240510230320_create_users.sql
+++ devenv/templates/webapp/backend/migrations/20240510230320_create_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
     id         TEXT      PRIMARY KEY NOT NULL,
-    username   TEXT      NOT NULL,
+    username   TEXT      NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP
