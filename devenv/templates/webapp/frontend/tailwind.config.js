4c1f75ec891a150f44c8c6ee4cbf2940afca3454
Rafael Fernández López <ereslibre@ereslibre.es>
2025-08-21 19:20:58 +0200

    Update

--- devenv/templates/webapp/frontend/tailwind.config.js
+++ devenv/templates/webapp/frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./frontend/**/*.{elm,html,js}'],
+    content: ['./**/*.{elm,html,js}'],
     theme: {
         extend: {},
     },
